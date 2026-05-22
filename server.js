68d50a7e59edc397ef36bbb3c827effcb9f785b8
robin-g-1997 <r.groen@agium.nl>
2026-05-22 15:04:20 +0200

    added password

--- server.js
+++ server.js
@@ -10,8 +10,35 @@ const analyzeRequirementsRoutes = require("./src/routes/analyze-requirements");
 const app = express();
 const port = process.env.PORT || 3000;
 
+function requirePassword(req, res, next) {
+  const appPassword = process.env.APP_PASSWORD;
+
+  if (!appPassword) {
+    return next();
+  }
+
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Basic ")) {
+    res.setHeader("WWW-Authenticate", 'Basic realm="CogniCheck"');
+    return res.status(401).send("Password required");
+  }
+
+  const encodedCredentials = authHeader.slice("Basic ".length);
+  const decodedCredentials = Buffer.from(encodedCredentials, "base64").toString("utf8");
+  const password = decodedCredentials.slice(decodedCredentials.indexOf(":") + 1);
+
+  if (password !== appPassword) {
+    res.setHeader("WWW-Authenticate", 'Basic realm="CogniCheck"');
+    return res.status(401).send("Invalid password");
+  }
+
+  next();
+}
+
 app.use(express.json({ limit: "10mb" }));
 app.use(cors());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(analyzeImageRoutes);
